e17234748d866b748143332f7b1696bd93ec57e5
Yash Sharma <ys909@snu.edu.in>
2022-05-08 15:07:45 +0530

    add search user and authorization functionality

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { registerUser, loginUser } from '../controllers/userController.js';
+import { registerUser, loginUser, allUsers } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router();
 
-router.post('/', registerUser)
+router.route('/').post(registerUser).get(protect, allUsers)
 router.post('/login', loginUser)
 
 export default router;
---
+++ middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/userModel.js'
+import asyncHandler from 'express-async-handler'
+
+export const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decodedToken)
+            req.user = await User.findById(decodedToken.id).select('-password');
+            next();
+        } catch (error) {
+            res.status(401);
+            throw new Error('Not authorised, token failed');
+        }
+    }
+
+    if (!token) {
+        res.status(401);
+        throw new Error('Not authorised, No token');
+    }
+})
