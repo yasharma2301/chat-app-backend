0d1178a020a8c8ce56d158cd5f6597c6bd5e55cd
yash <yash@criodo.com>
2023-09-24 13:26:49 +0530

    Allow cors

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-import express from 'express';
-import { config } from 'dotenv';
-import userRoutes from './Routes/userRoutes.js'
-import chatRoutes from './Routes/chatRoutes.js'
-import messageRoutes from './Routes/messageRoutes.js'
-import mongoose from 'mongoose';
-import { notFound, errorHandler } from './middleware/errorMiddlerware.js'
+import express from "express";
+import { config } from "dotenv";
+import userRoutes from "./Routes/userRoutes.js";
+import chatRoutes from "./Routes/chatRoutes.js";
+import messageRoutes from "./Routes/messageRoutes.js";
+import mongoose from "mongoose";
+import { notFound, errorHandler } from "./middleware/errorMiddlerware.js";
 import { Server } from "socket.io";
 import { createServer } from "http";
-import cors from 'cors'
+import cors from "cors";
 
 const app = express();
 const httpServer = createServer(app);
@@ -18,51 +18,57 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 const io = new Server(httpServer, {
-    pingTimeout: 60000,
-    cors: {
-        origin: 'https://letss-chatt.netlify.app'
-    }
+  pingTimeout: 60000,
+  cors: {
+    origin: "*",
+  },
 });
 const PORT = process.env.PORT || 5000;
 
-io.on('connection', (socket) => {
-    socket.on('setup', (userData) => {
-        socket.join(userData._id);
-        socket.emit('connected');
+io.on("connection", (socket) => {
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join_chat", (room) => {
+    socket.join(room);
+    console.log(`User joined the room ${room}`);
+  });
+
+  socket.on("new_message", (newMessageReceived) => {
+    let chat = newMessageReceived.chat;
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageReceived.sender._id) return;
+
+      socket.in(user._id).emit("message_received", newMessageReceived);
     });
+  });
 
-    socket.on('join_chat', (room) => {
-        socket.join(room);
-        console.log(`User joined the room ${room}`);
-    });
-
-    socket.on('new_message', (newMessageReceived) => {
-        let chat = newMessageReceived.chat;
-        if (!chat.users)
-            return console.log('chat.users not defined');
-
-        chat.users.forEach(user => {
-            if (user._id == newMessageReceived.sender._id) return;
-
-            socket.in(user._id).emit('message_received', newMessageReceived);
-        });
-    });
-
-    socket.on('typing', (room) => socket.in(room).emit('typing'));
-    socket.on('stop_typing', (room) => socket.in(room).emit('stop_typing'));
-})
-
-app.get('/', (req, res) => {
-    res.send(`Welcome to Let's Chat server!`);
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop_typing", (room) => socket.in(room).emit("stop_typing"));
 });
 
-app.use('/api/user', userRoutes);
-app.use('/api/chat', chatRoutes);
-app.use('/api/message', messageRoutes);
-app.use(notFound)
-app.use(errorHandler)
+app.get("/", (req, res) => {
+  res.send(`Welcome to Let's Chat server!`);
+});
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => httpServer.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message))
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    httpServer.listen(PORT, () =>
+      console.log(`Server running on port: ${PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
